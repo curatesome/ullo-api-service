85a2c5ade219e0940a9964304b9530eb04b54cf6
juahn.jeong <johnnyapu15@gmail.com>
2022-03-26 19:42:31 +0900

    데이터베이스 연결 url 수정

--- src/databases/index.ts
+++ src/databases/index.ts
@@ -1,3 +1,3 @@
 import { DB_HOST, DB_PORT, DB_USER, DB_PW } from '@config';
 
-export const dbConnection = `mongodb://${DB_USER}:${DB_PW}@${DB_HOST}:${DB_PORT}`;
+export const dbConnection = `mongodb://${DB_USER?DB_USER + ':' + DB_PW + '@':''}${DB_HOST}:${DB_PORT}`;
